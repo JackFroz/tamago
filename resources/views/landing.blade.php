24235f38fdce6cab96c31d4e275232e57ef4dd15
Muhammad Razzaaq Fadilah <abdurrazzaaqfad@gmail.com>
2021-05-30 21:38:06 +0700

    fix icon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{csrf_token()}}">
-        <title>Tamago</title>
-        <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
-        <link rel="icon" href="../images/logo-tamago.png">
-    </head>
-    <body>
-        <div id="app">
-            <app></app>
-        </div>
-        <script src="{{ mix('js/bootstrap.js') }}"></script>
-        <script src="{{ mix('js/app.js') }}"></script>
-    </body>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>Tamago</title>
+    <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="../images/logo_tamago.png">
+</head>
+
+<body>
+    <div id="app">
+        <app>
+        </app>
+    </div>
+    <script src="{{ mix('js/bootstrap.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
 </html>
