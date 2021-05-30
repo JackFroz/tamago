f8908b7b23b1669daee72db290b195ffc2b8e8fa
Muhammad Razzaaq Fadilah <abdurrazzaaqfad@gmail.com>
2021-05-30 21:36:42 +0700

    add route protection for vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,60 +1,91 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-import App from './pages/App'
-import Landing from './pages/Landing'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import ProjectBoard from './pages/ProjectBoard'
-import DivisionBoard from './pages/DivisionBoard'
+import App from "./pages/App";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ProjectBoard from "./pages/ProjectBoard";
+import DivisionBoard from "./pages/DivisionBoard";
 
 const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'landing',
-            component: Landing,
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: Login,
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: Register,
-        },
-        {
-            path: '/home',
-            name: 'home',
-            component: Home,
-        },
-        {
-            path: '/profile',
-            name: 'profile',
-            component: Profile,
-        },
-        {
-            path: '/project/:id',
-            name: 'project-board',
-            component: ProjectBoard,
-        },
-        {
-            path: '/division/:id',
-            name: 'division-board',
-            component: DivisionBoard,
-        },
-    ],
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "landing",
+      component: Landing,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: Home,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/project/:id",
+      name: "project-board",
+      component: ProjectBoard,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/division/:id",
+      name: "division-board",
+      component: DivisionBoard,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  if (requiresAuth) {
+    axios
+      .get("api/user", {
+        headers: { Authorization: "Bearer " + token },
+      })
+      .then(() => {
+        next();
+      })
+      .catch(() => {
+        next("login");
+      });
+  }
+
+  else next();
 });
 
 const app = new Vue({
-    el: '#app',
-    components: { App },
-    router,
+  el: "#app",
+  components: { App },
+  router,
 });
